7b73f9a7041679a553cf9e1a0d788c8b4343b852
waltzfordebby <lslestercayabyab@gmail.com>
2019-03-27 05:44:15 +0800

    Creating utils for home

--- mangadownloader/main/routes.py
+++ mangadownloader/main/routes.py
@@ -1,12 +1,12 @@
 from flask import render_template, request, Blueprint
-
+from mangadownloader.main.utils import hotMangaData
 main = Blueprint('main', __name__)
 
 
 @main.route("/")
 @main.route("/home")
 def home():
-    return render_template('main/home.html', title="Home")
+    return render_template('main/home.html', title="Home", hotmanga=hotMangaData())
 
 
 @main.route("/about")
--- mangadownloader/main/utils.py
+++ mangadownloader/main/utils.py
@@ -1,27 +1,77 @@
 import requests
 import threading
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
 
-def get_fresh_manga():
-    manga_panda_home = requests.get('https://www.mangapanda.com/')
-    manga_panda_home_html = BeautifulSoup(manga_panda_home.text, 'lxml')
+class RequestThread(threading.Thread):
+    def __init__(self, url):
+        super(RequestThread, self).__init__()
+        self._url = url
+        self._html = ''
 
-    latest_hot_manga = manga_panda_home_html.find(
-        id="latesthot").find_all("div", class_="latesthotimages")
-
-    manga_list = []
-    for div in latest_hot_manga:
-        manga_data = {
-            "home": div.find("h3").find("a").get("href"),
-            "chapter": div.find("h3").find("a").text
-        }
-        # print(div.find("h3").find("a").get("href"))
-        manga_list.append(manga_data)
-
-        # title.append(div.find('h3').find('a').text)
-
-    return manga_list
+    def run(self):
+        self._html = requests.get(self._url).text
 
 
-print(get_fresh_manga())
+class MangaPanda(object):
+    manga_panda_home_url = 'https://www.mangapanda.com'
+
+    def __init__(self):
+        self._manga_panda_home_request = requests.get(
+            'https://www.mangapanda.com')
+        self._manga_panda_home_html = BeautifulSoup(
+            self._manga_panda_home_request.text, 'lxml')
+        self._latest_hot_manga_html = self._manga_panda_home_html.find(
+            id="latesthot").find_all("div", class_="latesthotimages")
+        self._popular_manga_html = self._manga_panda_home_html.find(
+            "div", id="popularlist").find_all("li")
+
+    def get_hot_manga_chapter(self):
+        for manga in self._latest_hot_manga_html:
+            yield manga.find("h3").find("a").text
+
+    def get_hot_manga_url(self):
+        for manga in self._latest_hot_manga_html:
+            parsed_manga_url = urlparse(manga.find("h3").find("a").get("href"))
+            yield f'{parsed_manga_url.scheme}://{parsed_manga_url.netloc}/{parsed_manga_url.path.split("/")[1]}'
+
+    def get_each_hot_manga_html(self):
+        for url in self.get_hot_manga_url():
+            yield requests.get(url).text
+
+    def get_each_manga_cover_url(self):
+        for manga_html in self.get_each_hot_manga_html():
+            manga_home_html = BeautifulSoup(manga_html, 'lxml')
+            yield manga_home_html.find(id="mangaimg").find("img").get("src")
+
+    def get_hot_manga_summary(self):
+        for chapter, url in zip(self.get_hot_manga_chapter(), self.get_each_manga_cover_url()):
+            yield {'chapter': chapter, 'cover_url': url}
+
+    def get_popular_manga_url(self):
+        for manga in self._popular_manga_html:
+            yield f'{self.manga_panda_home_url}{manga.find("a").get("href")}'
+
+    def get_each_popular_manga_html(self):
+        for url in self.get_popular_manga_url():
+            yield requests.get(url).text
+
+    def get_popular_manga_cover_url(self):
+        for manga_html in self.get_each_popular_manga_html():
+            manga_home_html = BeautifulSoup(manga_html, 'lxml')
+            print(manga_home_html.find(id="mangaimg").find("img").get("src"))
+            # yield manga_home_html.find(id="mangaimg").find("img").get("src")
+
+
+def hotMangaData():
+    manga = MangaPanda()
+    return manga.get_hot_manga_summary()
+
+
+def test():
+    manga = MangaPanda()
+    manga.get_popular_manga_cover_url()
+
+
+# test()
