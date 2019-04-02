8587803a053a671b86e69aff7707f09f4bbd55c4
waltzfordebby <lslestercayabyab@gmail.com>
2019-04-02 22:55:38 +0800

    Created manga, author, and artist model

--- mangadownloader/models.py
+++ mangadownloader/models.py
@@ -17,3 +17,62 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+
+class Manga(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30), unique=True, nullable=False)
+    japanese_title = db.Column(
+        db.String(30), unique=True, nullable=False, default="Unknown")
+    type = db.Column(db.String(20), nullable=False, default="Unknown")
+    volumes = db.Column(db.Integer, nullable=False, default=0)
+    chapters = db.Column(db.Integer, nullable=False, default=0)
+    status = db.Column(db.String(20), nullable=False, default="Unknown")
+    # genre = db.relationship('Genre', backref='manga', lazy=True)
+    author_id = db.Column(db.Integer, db.ForeignKey(
+        'author.id'), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey(
+        'artist.id'), nullable=False)
+    serialization = db.Column(db.Text, nullable=False, default="Unknown")
+    created_on = db.Column(db.DateTime, server_default=db.func.now())
+    updated_on = db.Column(
+        db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+    def __repr__(self):
+        return f"Manga('{self.title}','{self.author_id}','{self.type}')"
+
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    manga = db.relationship('Manga', backref="author", lazy=True)
+    last_name = db.Column(db.String(30), nullable=False, default="None")
+    first_name = db.Column(db.String(30), nullable=False, default="None")
+    pen_name = db.Column(db.Text, nullable=False, default="Unknown")
+    birth_date = db.Column(db.DateTime, nullable=False, default="Unknown")
+    birth_place = db.Column(db.Text, nullable=False, default="Unknown")
+    weight = db.Column(db.Integer, nullable=False, default="Unknown")
+    blood_type = db.Column(db.Integer, nullable=False, default="Unknown")
+    created_on = db.Column(db.DateTime, server_default=db.func.now())
+    updated_on = db.Column(
+        db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+    def __repr__(self):
+        return f"Author('{self.last_name}','{self.first_name}','{self.pen_name}')"
+
+
+class Artist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    manga = db.relationship('Manga', backref="artist", lazy=True)
+    last_name = db.Column(db.String(30), nullable=False, default="None")
+    first_name = db.Column(db.String(30), nullable=False, default="None")
+    pen_name = db.Column(db.Text, nullable=False, default="Unknown")
+    birth_date = db.Column(db.DateTime, nullable=False, default="Unknown")
+    birth_place = db.Column(db.Text, nullable=False, default="Unknown")
+    weight = db.Column(db.Integer, nullable=False, default="Unknown")
+    blood_type = db.Column(db.Integer, nullable=False, default="Unknown")
+    created_on = db.Column(db.DateTime, server_default=db.func.now())
+    updated_on = db.Column(
+        db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+    def __repr__(self):
+        return f"Author('{self.last_name}','{self.first_name}','{self.pen_name}')"
